d986a48f2bfafc3aae5453d75ea481f4f973cfc5
Andrei <andreibobirica99@gmail.com>
2022-04-11 23:58:16 +0200

    fix head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,19 +10,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link
-            rel="alternate"
-            type="application/rss+xml"
-            title="RSS"
-            href="https://www.avneesh.tech/rss.xml"
-          />
-          <meta
-            name="keywords"
-            content="Avneesh, Agarwal, Avneesh Agarwal, web dev, blogger, content creator"
-          />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png"></link>
-          <meta name="theme-color" content="#fff" />
         </Head>
         <body>
           <Main />
